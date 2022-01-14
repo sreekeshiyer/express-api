b0573a443ca0dda3321ee2f79c43bb1d56775c19
sreekeshiyer <sreekesh.subramanian@gmail.com>
2022-01-14 10:54:56 +0530

    ✨REST API with Express.js ✨

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 const { engine } = require("express-handlebars");
-const logger = require("./middleware/logger");
 const todos = require("./todos");
 
 const app = express();
